8eaca13f6a48b7d96111e1f5e281f7629dfa44d6
vj1701-dot <vj1701@sampark.email>
2025-07-18 11:00:54 -0700

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,7 +1,10 @@
 import random
 import string
+import secrets
 
 def generate_uid():
     prefix = ''.join(random.choices(string.ascii_uppercase, k=2))
     suffix = ''.join(random.choices(string.digits, k=4))
     return f"{prefix}{suffix}"
+
+print(secrets.token_urlsafe(32))
